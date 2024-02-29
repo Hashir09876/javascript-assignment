3dc1e6eb19a80ec7fde4ef7cbb2602068abda127
Hashir09876 <khashirhasho4@gmail.com>
2024-02-29 15:30:39 +0500

    assignmet done

---
+++ arithmatic operators/app.js
@@ -0,0 +1,127 @@
+
+
+alert("hello")
+
+//assignments
+
+//q1
+
+var x=25
+var y=7
+
+var answer1=x%y
+console.log(answer1)
+
+//q2
+
+var a=25
+var b=2
+
+var answer2=a*b
+console.log(answer2)
+
+//q3
+
+var c=12
+var d=3
+
+var answer3=c ** d
+console.log(answer3)
+
+//q4
+
+var e=10
+var f=3
+
+var answer4=e +=f*2
+console.log(answer4)
+
+//q5
+
+var g=25
+var h=4
+
+var answer5=g/h
+console.log(answer5)
+
+//q6
+
+var i=6
+var j=3
+
+var answer6=i*j
+console.log(answer6)
+
+//q7
+
+var k=18
+var l=5
+
+var answer7=k-l+2
+console.log(answer7)
+
+//q8
+
+var m=7
+var n=2
+
+var answer8=m ** n
+console.log(answer8)
+
+//q9
+
+var o=13
+var p=4
+
+var answer9=o%(p+2)
+console.log(answer9)
+
+//q10
+
+var q=8
+var r=3
+
+var answer10=q*r
+console.log(answer10)
+
+//q11
+
+var s=24
+var t=3
+
+var answer11=s/t
+console.log(answer11)
+
+//q12
+
+var  u=11
+var v=5
+
+var answer12=u-v
+console.log(answer12)
+
+//q13
+
+var w=22
+var x=5
+
+var answer13=w+(x**2)
+console.log(answer13)
+
+//q14
+
+var y=19
+var z=4
+
+var answer14=y%(z+1)
+console.log(answer14)
+
+//q15
+
+var a=10
+var b=3
+
+var answer15=a*(b-2)
+console.log(answer15)
+
+
